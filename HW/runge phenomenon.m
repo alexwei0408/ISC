e8bb43afb8a48b424d0f4d10debb5f730ddcd7d6
alexwei0408 <jinwei.sc11@nycu.edu.tw>
2025-09-09 21:11:12 +0800

    修改函數為1/（1+x^2)

--- HW/runge phenomenon.m
+++ HW/runge phenomenon.m
@@ -3,33 +3,31 @@ function runge
     hold on; 
     grid on; 
     box on;
-    axis([-5 5 -10 10]);
-    title('Runge phenomenon for x in [-5,5]'); 
 
-    f = @(x) 1./(1+x);
+    axis([-5 5 -0.1 1.1]);                         
+    title('Polynomial interpolation with uniform nodes on [-5,5]'); 
 
-    % 函數曲線（避開 x=-1）
-    x = linspace(-5,5,4000);
-    x = x(abs(x+1) > 1e-6);   % 去掉接近 -1 的點
+    f = @(x) 1./(1 + (x.^2));
+    x = linspace(-5, 5, 4000);
     y = f(x);
-    plot(x, y, 'r', 'LineWidth', 2);
-    text(0.05,0.9,'f(x)=1/(1+x)','Units','normalized','Color','r');
+    plot(x, y, 'r', 'LineWidth', 2, 'DisplayName', 'f(x)=1/(1+x^2)');
+    text(0.05, 0.9, 'f(x)=1/(1+x^2)', 'Units','normalized', 'Color','r');
 
-    for n = [6 10 14]           % 設定節點數
-        x_nodes = linspace(-5,5,n);
-        x_nodes = x_nodes(abs(x_nodes+1) > 1e-8); % 刪掉 -1
+
+    for n = [6 10 14]                
+        x_nodes = linspace(-5, 5, n);
         y_nodes = f(x_nodes);
 
-        p = polyfit(x_nodes, y_nodes, numel(x_nodes)-1);
-        f_interp = polyval(p, x);
+        p = polyfit(x_nodes, y_nodes, n-1);   
+        y_interp = polyval(p, x);
 
-        n_nodes = numel(x_nodes);
-        label_str = sprintf('%d nodes', n_nodes);
-        h = plot(x, f_interp);
-        set(h, 'LineWidth', 1.2);
-        set(h, 'DisplayName', label_str);
+        label_str = sprintf('%d nodes (deg %d)', n, n-1);
+        h = plot(x, y_interp, 'LineWidth', 1.2, 'DisplayName', label_str);
+
+        plot(x_nodes, y_nodes, 'o', 'MarkerSize', 4, 'HandleVisibility','off', ...
+             'Color', h.Color);
     end
 
-    legend('f(x)','6 nodes','10 nodes','14 nodes','Location','best');
+    legend('Location','best');
     hold off;
 end
